9698a8bc45cf621c375bdf8dc368523a8f230655
nimaprmdi <nimaprmdi@gmail.com>
2022-10-19 17:06:40 +0330

    Add middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
-import { createStore } from "@reduxjs/toolkit";
+import { applyMiddleware, createStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
+import logger from "redux-logger";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
